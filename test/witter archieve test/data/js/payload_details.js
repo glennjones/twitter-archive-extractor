8a9afca033e7d98a3a8113a50faa45339977b899
Glenn Jones <glennjonesnet@gmail.com>
2013-12-27 16:55:30 +0000

    Changes to allow overloading of imported archives

---
+++ test/witter archieve test/data/js/payload_details.js
@@ -0,0 +1,5 @@
+var payload_details =  {
+  "tweets" : 2008,
+  "created_at" : "2013-12-06 10:45:26 +0000",
+  "lang" : "en"
+}
---
+++ test/witter archieve test/data/js/tweets/2013_11.js
@@ -0,0 +1,24 @@
+Grailbird.data.tweets_2013_11 = 
+[ {
+  "source" : "test system",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "406054311244746752",
+  "text" : "This is a test tweet in a test archieve file",
+  "id" : 406054311244746752,
+  "created_at" : "2013-11-28 13:37:46 +0000",
+  "user" : {
+    "name" : "Glenn Jones",
+    "screen_name" : "glennjones",
+    "protected" : false,
+    "id_str" : "946491",
+    "profile_image_url_https" : "https:\/\/pbs.twimg.com\/profile_images\/1442481406\/Glenn_Jones_normal.png",
+    "id" : 946491,
+    "verified" : false
+  }
+}, ]
